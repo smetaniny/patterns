eab867e08478df47d46695e68cc6a3d50966fa43
Sergey <sm.sergey.v@yandex.ru>
2023-10-04 12:46:27 +0300

    Lesson 5

---
+++ app/Http/Controllers/LavrikOOP/Lesson5/Modules/Articles/Views/v_item.php
@@ -0,0 +1,6 @@
+<div class="article-item">
+	<h2><?=$article['title']?></h2>
+	<div><?=$article['content']?></div>
+</div>
+<hr>
+---------------------
---
+++ app/Http/Controllers/LavrikOOP/Lesson5/System/Router.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace System;
+
+use System\Contracts\IRouter;
+use System\Exceptions\Exc404;
+
+class Router implements IRouter{
+	protected string $baseUrl;
+	protected int $baseShift;
+	protected array $routes = [];
+
+	public function __construct(string $baseUrl = ''){
+		$this->baseUrl = $baseUrl;
+		$this->baseShift = strlen($this->baseUrl);
+	}
+
+	public function addRoute(string $url, string $contorllerName, string $contorllerMethod = 'index') : void{
+		$this->routes[] = [
+			'path' => $url,
+			'c' => $contorllerName,
+			'm' => $contorllerMethod
+		];
+	}
+
+	public function resolvePath(string $url) : array{
+		$relativeUrl = substr($url, $this->baseShift);
+		$route = $this->findPath($relativeUrl);
+		$params = explode('/', $relativeUrl);
+		$controller = new $route['c']();
+		$controller->setEnviroment($params);
+		// var_dump($controller instanceof IController); // hmmmm
+
+		return [
+			'controller' => $controller,
+			'method' => $route['m']
+		];
+	}
+
+	protected function findPath(string $url) : array{
+		$activeRoute = null;
+
+		foreach($this->routes as $route){
+			if($url === $route['path']){
+				$activeRoute = $route;
+			}
+		}
+
+		if($activeRoute === null){
+			throw new Exc404('route not found');
+		}
+
+		return $activeRoute;
+	}
+}
