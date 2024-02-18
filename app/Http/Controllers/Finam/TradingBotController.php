20d7b2c83fa87f01dd086279a146b4aa86f656c9
Sergey <sergey@skvnet.ru>
2024-02-18 08:59:56 +0300

    PatternsDesignGangFour

--- app/Http/Controllers/Finam/TradingBotController.php
+++ app/Http/Controllers/Finam/TradingBotController.php
@@ -6,41 +6,47 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Http;
 use App\Http\Controllers\Controller;
 use Illuminate\Support\Facades\Log;
+use Inertia\Inertia;
 
 class TradingBotController extends Controller
 {
-    private $accessToken = "CAEQwqW7ARoYututz/AooiR0JUgQiLMVDNj9WWOOfNay";
-//    private $accessToken = "CAEQwqW7ARoYututz/AooiR0JUgQiLMVDNj9WWOOfNay";
+    private string $accessToken = "CAEQwqW7ARoYututz/AooiR0JUgQiLMVDNj9WWOOfNay";
 
     public function executeBot()
     {
         $securityBoard = "FUT"; // Символ акции (Apple)
         $symbol = "SVH4"; // Символ акции (Apple)
-        $timeframe = "D1"; // Временной интервал
-        $intervalFrom = "2023-12-11";
-        $intervalTo = "2024-02-15";
-        $intervalCount = 100;
+        $timeframe = "H1"; // Временной интервал
+        $intervalCount = 100000;
+
+        // Получаем текущую дату
+        $currentDate = date('Y-m-d');
+        // Получаем дату 30 дней назад от текущей даты
+        $thirtyDaysAgo = date('Y-m-d', strtotime('-10 days', strtotime($currentDate)));
+
+        // Используем полученные даты как интервал
+        $intervalFrom = $thirtyDaysAgo;
+        $intervalTo = $currentDate;
+
+
         // Обработка данных
         $candles = $this->getDayCandles($symbol, $timeframe, $intervalFrom, $intervalTo, $intervalCount, $securityBoard);
 
-        // Рассчитываем скользящее среднее для закрытых цен за 10 дней
         $calculateEMA10 = $this->calculateEMA($candles, 10);
         $calculateEMA40 = $this->calculateEMA($candles, 40);
-       dd( $this->findCrossOvers($calculateEMA10, $calculateEMA40));
-
-        return response()->json($candles);
+        return Inertia::render('Welcome', [
+            'redData' => $calculateEMA10,
+            'blueData' => $calculateEMA40,
+        ]);
     }
+
 
     private function getDayCandles($symbol, $timeframe, $intervalFrom, $intervalTo, $intervalCount, $securityBoard)
     {
         $response = Http::withHeaders([
             'X-Api-Key' => $this->accessToken
-        ])->get("https://trade-api.finam.ru/public/api/v1/day-candles?SecurityBoard=$securityBoard&SecurityCode=$symbol&TimeFrame=$timeframe&Interval.From=$intervalFrom&Interval.To=$intervalTo&Interval.Count=$intervalCount");
+        ])->get("https://trade-api.finam.ru/public/api/v1/intraday-candles?SecurityBoard=$securityBoard&SecurityCode=$symbol&TimeFrame=$timeframe&Interval.From=$intervalFrom&Interval.To=$intervalTo&Interval.Count=$intervalCount");
 
-//        $response = Http::withHeaders([
-//            'X-Api-Key' => $this->accessToken
-//        ])->get("https://trade-api.finam.ru/public/api/v1/securities");
-        // Проверка успешного получения данных свечей
         if ($response->successful()) {
             return $response->json();
         } else {
@@ -53,66 +59,56 @@ class TradingBotController extends Controller
     private function calculateEMA($candles, $period)
     {
         $closePrices = [];
-        foreach ($candles['data']['candles'] as $candle) {
-            $closePrices[$candle['date']] = $candle['close']['num'] / 100;
+        $dates = [];
+
+
+        // Проверка наличия данных о свечах
+        if (!isset($candles['data']['candles']) || !is_array($candles['data']['candles'])) {
+            return []; // Возвращаем пустой массив, если данные отсутствуют или имеют неправильный формат
         }
+
+        foreach ($candles['data']['candles'] as $candle) {
+            // Проверка наличия необходимых данных в свече
+            if (!isset($candle['timestamp'], $candle['close']['num'])) {
+                continue; // Пропускаем эту свечу, если отсутствует необходимая информация
+            }
+
+            $dates[] = $candle['timestamp'];
+            $closePrices[] = $candle['close']['num'] / 100;
+        }
+
         $ema = [];
+        $count = count($closePrices);
 
-        // Находим первую доступную дату и устанавливаем ее значение цены закрытия в качестве начального значения EMA
-        $firstDate = array_key_first($closePrices);
-        $ema[$firstDate] = $closePrices[$firstDate];
+        // Проверка, достаточно ли данных для расчета EMA
+        if ($count < $period) {
+            return []; // Возвращаем пустой массив, если данных недостаточно для расчета EMA
+        }
 
-        // Рассчет коэффициента сглаживания
-        $alpha = 2 / ($period + 1);
+        $multiplier = 2 / ($period + 1); // Вычисляем множитель взвешивания
 
-        // Вычисление EMA для каждого последующего дня
-        $dates = array_keys($closePrices);
-        for ($i = 1; $i < count($dates); $i++) {
-            $currentDate = $dates[$i];
-            // Рассчитываем EMA используя предыдущее значение EMA и текущую цену
-            $ema[$currentDate] = ($closePrices[$currentDate] - $ema[$dates[$i - 1]]) * $alpha + $ema[$dates[$i - 1]];
+        // Шаг 1: Рассчитываем начальное простое скользящее среднее (SMA)
+        $sma = array_sum(array_slice($closePrices, 0, $period)) / $period;
+
+        // Заполняем начальные значения EMA
+        for ($i = 0; $i < $period; $i++) {
+            $ema[$dates[$i]] = $closePrices[$i];
+        }
+
+        // Шаг 3: Рассчитываем EMA для последующих дней
+        for ($i = $period; $i < $count; $i++) {
+            // Проверяем, есть ли данные для предыдущей даты
+            if (isset($ema[$dates[$i - 1]])) {
+                // Рассчитываем EMA с использованием формулы
+                $emaValue = ($closePrices[$i] - $ema[$dates[$i - 1]]) * $multiplier + $ema[$dates[$i - 1]];
+                // Сохраняем рассчитанное значение EMA с соответствующей датой
+                $ema[$dates[$i]] = $emaValue;
+            } else {
+                // Если данные для предыдущей даты отсутствуют, пропускаем эту итерацию
+                continue;
+            }
         }
 
         return $ema;
     }
-
-    private function findCrossOvers($ema10, $ema40)
-    {
-        // Получаем список дат, которые есть в обоих массивах
-        $dates = array_intersect(array_keys($ema10), array_keys($ema40));
-
-        // Инициализируем массив для хранения дат пересечений
-        $crossOvers = [];
-
-        // Проходимся по каждой дате
-        foreach ($dates as $date) {
-            // Проверяем пересечение EMA 10 и EMA 40 для текущей даты
-            if ($ema10[$date] > $ema40[$date]) {
-                // EMA 10 пересекла EMA 40 снизу вверх
-                $crossOvers[$date] = 'EMA 10 пересекла EMA 40 сверху вверх';
-            } elseif ($ema10[$date] < $ema40[$date]) {
-                // EMA 10 пересекла EMA 40 сверху вниз
-                $crossOvers[$date] = 'EMA 10 пересекла EMA 40 снизу вниз';
-            }
-        }
-
-        // Переводим цены в сотые
-        foreach ($ema10 as $date => $price) {
-            $ema10[$date] = number_format($price / 100, 2);
-        }
-
-        foreach ($ema40 as $date => $price) {
-            $ema40[$date] = number_format($price / 100, 2);
-        }
-
-        return [
-            'crossOvers' => $crossOvers,
-            'ema10' => $ema10,
-            'ema40' => $ema40
-        ];
-    }
-
-
-
-
 }
--- resources/js/Pages/Welcome.jsx
+++ resources/js/Pages/Welcome.jsx
@@ -1,10 +1,112 @@
 import React from 'react';
-import RealtimeChart from "@/Pages/Chart";
+import {
+    Chart as ChartJS,
+    CategoryScale,
+    LinearScale,
+    PointElement,
+    LineElement,
+    Title,
+    Tooltip,
+    Legend,
+} from 'chart.js';
+import { Line } from 'react-chartjs-2';
 
-export default function Welcome() {
+ChartJS.register(
+    CategoryScale,
+    LinearScale,
+    PointElement,
+    LineElement,
+    Title,
+    Tooltip,
+    Legend
+);
+
+export const options = {
+    responsive: true,
+    interaction: {
+        mode: 'index' ,
+        intersect: false,
+    },
+    stacked: false,
+    plugins: {
+        title: {
+            display: true,
+            text: 'Chart.js Line Chart - Multi Axis',
+        },
+    },
+    scales: {
+        y: {
+            type: 'linear' ,
+            display: true,
+            position: 'left' ,
+        },
+        y1: {
+            type: 'linear' ,
+            display: true,
+            position: 'right' ,
+            grid: {
+                drawOnChartArea: false,
+            },
+        },
+    },
+};
+
+const redData = {
+    "2024-02-02": 2025.6787787719,
+    "2024-02-05": 1699.0099099043,
+    "2024-02-06": 1807.0081081035,
+    "2024-02-07": 1892.6429975393,
+    "2024-02-08": 1967.6169979867,
+    "2024-02-09": 2028.4139074436,
+    "2024-02-12": 2079.4295606357,
+    "2024-02-13": 2110.8060041565,
+    "2024-02-14": 1768.2958215826,
+    "2024-02-15": 1868.9693085676,
+    "2024-02-16": 1959.8839797371
+};
+
+const blueData = {
+    "2024-02-02": 2025.6787787719,
+    "2024-02-05": 1699.0099099043,
+    "2024-02-06": 1807.0081081035,
+    "2024-02-07": 1892.6429975393,
+    "2024-02-08": 1967.6169979867,
+    "2024-02-09": 2028.4139074436,
+    "2024-02-12": 2079.4295606357,
+    "2024-02-13": 2110.8060041565,
+    "2024-02-14": 1768.2958215826,
+    "2024-02-15": 1868.9693085676,
+    "2024-02-16": 2020.8839797371
+};
+
+
+
+export default function Welcome({redData = {}, blueData = {}}) {
+    const data = {
+        labels: Object.keys(redData), // Используем ключи из одного из наборов данных
+        datasets: [
+            {
+                label: 'Dataset 1',
+                data: Object.values(redData), // Используем значения из набора данных для красной линии
+                borderColor: 'rgb(255, 99, 132)',
+                backgroundColor: 'rgba(255, 99, 132, 0.5)',
+                yAxisID: 'y',
+            },
+            {
+                label: 'Dataset 2',
+                data: Object.values(blueData), // Используем значения из набора данных для синей линии
+                borderColor: 'rgb(53, 162, 235)',
+                backgroundColor: 'rgba(53, 162, 235, 0.5)',
+                yAxisID: 'y1',
+            },
+        ],
+    };
+
     return (
         <div>
-            <RealtimeChart />
+            <Line options={options} data={data} />
         </div>
     );
 }
+
+
